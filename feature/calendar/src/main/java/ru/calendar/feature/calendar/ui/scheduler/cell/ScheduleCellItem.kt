2277d92bf6999902081e769393c2cb1dc148b38d
Sergei Pokhodai <s.pokhodai@detmir.ru>
2025-02-11 10:38:42 +0400

    event item

---
+++ feature/calendar/src/main/java/ru/calendar/feature/calendar/ui/scheduler/cell/ScheduleCellItem.kt
@@ -0,0 +1,8 @@
+package ru.calendar.feature.calendar.ui.scheduler.cell
+
+class ScheduleCellItem {
+
+    interface View {
+
+    }
+}
---
+++ feature/calendar/src/main/java/ru/calendar/feature/calendar/ui/scheduler/cell/ScheduleCellItemView.kt
@@ -0,0 +1,14 @@
+package ru.calendar.feature.calendar.ui.scheduler.cell
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ScheduleCellItemView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr), ScheduleCellItem.View {
+
+
+}
